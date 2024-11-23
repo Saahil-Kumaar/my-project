2c715f796b5c79dc0e30079c0ec5268638ee18cd
Saahil-Kumaar <sk7593277@gmail.com>
2024-11-23 15:23:26 +0530

    Initial

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/* Not Preffered using namespace std */
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"HEllo World\n";
+    return 0;
+}
+*/
+/* Better way to use std is: */
+#include<iostream>
+int main(){
+    std::cout<<"HEllo World\n";
+    return 0;
+}
